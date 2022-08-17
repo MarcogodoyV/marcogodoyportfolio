22506b977bc5c19fe8b22a6d9c9e730afa5987e6
Marco Godoy <marcogodoyvalbuena@gmail.com>
2022-08-17 20:44:45 -0300

    WIP

--- client/src/components/main/about.js
+++ client/src/components/main/about.js
@@ -6,8 +6,7 @@ const About = () => {
                 <div className="about__container container grid">
                     <img src={require("../../assets/images/about.jpeg")} alt="" className="about__img"></img>
                     <div className="about__data">
-                        <p className="about__description">I'm an electronic engineer from Venezuela living in Argentina. I started learning 
-                        web development in 2020 in my free time and since that I got more and more involve in the area, to the point I decided to switched my career path and start freelancing. I'm Able to effectively self-manage during independent projects, as well as collaborate in a team setting in order to get the work done in the best way possible.</p>
+                        <p className="about__description">I'm an electronic engineer from Venezuela living in Argentina. In 2020 I started learning web development in my free time, and since then I got more and more involved in the area, to the point in which I decided to switch my career path and start freelancing. Now I'm able to effectively self-manage during independent projects, and collaborate in a team setting in order to get the work done in the best way possible.</p>
                         <div className="about__info">
                             <div>
                                 <span className="about__info-title">01+</span>
